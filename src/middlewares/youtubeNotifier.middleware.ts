7c317835b9df9890712ef1d5b95c7f160874aa26
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-06-26 17:24:49 -0700

    fixed to channelId

--- src/middlewares/youtubeNotifier.middleware.ts
+++ src/middlewares/youtubeNotifier.middleware.ts
@@ -3,7 +3,7 @@ import YoutubeNotifier from "../core/feed/services/youtubeNotifier.service";
 import g1 from "../../prisma/seed/generation1.json";
 
 module.exports = (app) => {
-  const subscription = [...g1.members.map(member => member.id), "UCZpMTTPDp2EAev6nb68Onjg"];
+  const subscription = [...g1.members.map(member => member.channel.id), "UCZpMTTPDp2EAev6nb68Onjg"];
   const notifier: YoutubeNotifier = new YoutubeNotifier(subscription)
 
   app.use('/feeds-youtube', notifier.start());
