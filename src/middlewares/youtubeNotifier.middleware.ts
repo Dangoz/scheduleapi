571c83c2d907fd2a7e994d000c829f7c5c26768f
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-06-26 17:22:44 -0700

    subscription to pc members

--- src/middlewares/youtubeNotifier.middleware.ts
+++ src/middlewares/youtubeNotifier.middleware.ts
@@ -1,26 +1,10 @@
 import YouTubeNotifier from "youtube-notification";
 import YoutubeNotifier from "../core/feed/services/youtubeNotifier.service";
+import g1 from "../../prisma/seed/generation1.json";
 
 module.exports = (app) => {
-  
-  const subscription = ['UCZpMTTPDp2EAev6nb68Onjg', 'UCkVWy3Y2aBSi8NfZ7oIrfGg', 'UCB7sSUNwh_dXE7ZL3DsGDpw'];
+  const subscription = [...g1.members.map(member => member.id), "UCZpMTTPDp2EAev6nb68Onjg"];
   const notifier: YoutubeNotifier = new YoutubeNotifier(subscription)
 
   app.use('/feeds-youtube', notifier.start());
-
-  // sample json structure from 'notified' event
-  const data = {
-    video: {
-      id: "AmadfVM-bR4",
-      title: "test0.1.0",
-      link: "https://www.youtube.com/watch?v=AmadfVM-bR4"
-    },
-    channel: {
-      id: "UCZpMTTPDp2EAev6nb68Onjg",
-      name: "J Z",
-      link: "https://www.youtube.com/channel/UCZpMTTPDp2EAev6nb68Onjg"
-    },
-    published: "2021-06-16T20:49:23.000Z",
-    updated: "2021-06-16T20:49:47.122Z"
-  }
 }
